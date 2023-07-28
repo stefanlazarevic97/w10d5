98ce0a3926c6d138a9e3b651e4c4a06eba142961
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-28 16:05:07 -0700

    add phase 7

---
+++ art-museum/src/Root.jsx
@@ -0,0 +1,9 @@
+import App from "./App";
+
+function Root() {
+    return (
+        <App />
+    );
+};
+
+export default Root;
--- art-museum/src/index.js
+++ art-museum/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Root from './Root';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <Root />
+        </BrowserRouter>
+    </React.StrictMode>,
   document.getElementById('root')
 );
